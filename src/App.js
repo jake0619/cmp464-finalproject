c594d3fb53e4edab8093288c87d42047ba4ce41f
Jayke Lim <jake0619@gmail.com>
2022-05-21 20:26:15 -0400

    started nav bar functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import kanyeLogo from "./images/kanyeIcon.png";
+import { NavLink } from "react-router-dom";
 
 function App() {
   const [quote, setQuote] = useState("");
@@ -26,6 +27,38 @@ function App() {
         src={kanyeLogo}
         onClick={() => getQuote()}
       />
+      <nav className="navBar">
+        <NavLink
+          style={({ isActive }) => {
+            return {
+              color: isActive ? "red" : "",
+            };
+          }}
+          to={`/About`}
+        >
+          About
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => {
+            return {
+              color: isActive ? "red" : "",
+            };
+          }}
+          to={`/Resume`}
+        >
+          Resume
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => {
+            return {
+              color: isActive ? "red" : "",
+            };
+          }}
+          to={`/Weather`}
+        >
+          Weather
+        </NavLink>
+      </nav>
     </div>
   );
 }
