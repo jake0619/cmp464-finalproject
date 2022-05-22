67896203e1c17dc6f20b4671f641e8ff493039e1
Jayke Lim <jake0619@gmail.com>
2022-05-21 21:31:43 -0400

    added weather zipcode input form with validator

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,15 @@ export default function About(){
   console.log("in about section");
   return(
     <div>
-      <p>About Section</p>
+      <h1>Jayke Lim Final Project</h1>
+      <h3>Tools used:</h3>
+      <ul>
+        <li>NPM</li>
+        <li>React</li>
+        <li>Hooks (useState, useEffect)</li>
+        <li>React Router DOM</li>
+        <li>Fetch</li>
+      </ul>
     </div>
   )
 }
--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,22 +1,44 @@
 import {useState} from 'react'
+import WeatherData from './WeatherData';
 
 export default function Weather(){
   const[zipCode, setZipCode] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleZipChange = (event) =>{
-    setZipCode(event.target.value);
-    console.log(event.target.value);
+  const handleSubmitZip = (event) =>{
+    event.preventDefault();
+    let zipcode = event.target[0].value;
+    console.log(event);
+    console.log("checking if valid zip code: "+event.target.value);
+    if(isValidZipcode(zipcode)){
+      setZipCode(zipCode);
+      
+      setErrorMessage("");
+    }
+    else{
+      console.error("Please input a valid 5 digit US zipcode");
+      setErrorMessage("Not a valid 5 digit US zipcode: "+zipcode);
+    }
+    
   }
+
+  const isValidZipcode = (sZip) =>{
+    return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(sZip);
+  }
+
+
+
 
   return(
     <div className='weatherContainer'>
       <h1>Weather</h1>
       <h3>Search for your local weather by inputting your zip code</h3>
-      <form>
-        
-        <input type="text" placeholder='zipcode' onChange={handleZipChange}></input>
+      {errorMessage && <div className="error"> {errorMessage} </div>}
+      <form onSubmit={handleSubmitZip}>
+        <input type="text" placeholder='zipcode'></input>
         <input type="submit"></input>
       </form>
+      <WeatherData/>
     </div>
   )
 }
