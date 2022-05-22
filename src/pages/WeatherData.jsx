67295383af27a0c9deaf7557aca88ab9d16632f8
Jayke Lim <jake0619@gmail.com>
2022-05-21 23:46:59 -0400

    updated weather data api, implemented weather data page

--- src/pages/WeatherData.jsx
+++ src/pages/WeatherData.jsx
@@ -3,34 +3,53 @@ import { useParams } from 'react-router-dom';
 import config from '../config';
 
 export default function WeatherData(){
-  const [city, setCity] = useState("");
-  const [state, setState] = useState("");
-  const [lat, setLat] = useState("");
-  const [long, setLong] = useState("");
-
+  const [weatherLoaded, setWeatherLoaded] = useState(false);
+  const [city ,setCity] = useState("");
+  const [region, setRegion] = useState("");
+  const [feelsLike ,setfeelsLike] = useState("");
+  const [temp ,setTemp] = useState("");
+  const [wind ,setWind] = useState("");
+  const [img, setImg] = useState("");
+  const [time, setTime] = useState("");
+  
   let params = useParams();
   let {zipCode} = params;
-  
 
+  const getWeatherData = async (zipcode) => {
+    let api_key = config.api.weather;
+    let response = await fetch("https://api.weatherapi.com/v1/current.json?key="+api_key+"&q="+zipcode+"&aqi=no")
+    let result = await response.json();
+    let {location: {name: city, region, localtime}, current} = result;
+    let {condition, temp_f, wind_mph, } = current
+    let {text: feelsLike, icon} = condition
+    setCity(city);
+    setRegion(region);
+    setTime(localtime);
+    setTemp(temp_f)
+    setWind(wind_mph)
+    setfeelsLike(feelsLike)
+    setImg(icon)
+    setWeatherLoaded(true);
+  }
+  
   useEffect(()=>{
     console.log("in weather data, zipcode input: "+zipCode);
-    getLocationDataFromZipCode(zipCode);
+    //getLocationDataFromZipCode(zipCode);
+    getWeatherData(zipCode);
   },[zipCode])
 
-  const getLocationDataFromZipCode = async (zip) => {
-    let api_key = config.api.zipcode
-    //'https://thezipcodes.com/api/v1/search?zipCode=13040&countryCode=US&apiKey=84cef520019881374ea852f8f8291f0e'
-    let response = await fetch("https://thezipcodes.com/api/v1/search?zipCode="+zip+"&countryCode=US&apiKey="+api_key)
-                        .catch((err) => {console.error(err)});
-    let {success, location} = await response.json();
-    let {city, state, longitude, latitude} = location[0];
-    setCity(city)
-    setState(state)
-    setLat(latitude)
-    setLong(longitude)
-    console.log(city);
-  }
-
-  // getLocationDataFromZipCode(zipCode);
-  
+  return(
+    <div>
+      {weatherLoaded && 
+      <div className='weatherDetailContainer'>
+        <h1 style={{marginBottom: 0}}>{city}, {region}</h1>
+        <h5 style={{marginTop: 0}}>{time}</h5>
+        <img src={img}></img>
+        <h3>{feelsLike}</h3>
+        <h3>Temperature: {temp}ºF</h3>
+        <h3>Wind: {wind} MPH</h3>
+      </div>
+      }
+    </div>
+  )
 }
